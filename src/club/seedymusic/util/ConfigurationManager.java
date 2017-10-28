75dbc33c172672fb128e1233e020db500a058c93
Sultaneous <karimsultan@hotmail.com>
2017-10-28 17:22:50 -0400

    Completed Javadoc comments for ConfigurationManager class

--- src/club/seedymusic/util/ConfigurationManager.java
+++ src/club/seedymusic/util/ConfigurationManager.java
@@ -5,12 +5,33 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+/**
+ * <h2>ConfigurationManager Class</h2>
+ * <p>
+ * This class is responsible for loading the properties for DAO fields, if the file is available. If
+ * the file is not found, it continues with caller provided default values.
+ * 
+ * @author Karim Sultan
+ * 
+ * @version Oct 25, 2017 Created this class.
+ */
 public class ConfigurationManager
 {
+   /**
+    * The name of the configuration file.
+    */
    private static final String CONFIG_FILE = "dao.configuration.properties";
 
+   /**
+    * Stores the properties read from the file.
+    */
    private Properties configuration;
 
+   /**
+    * 
+    * Constructs a new ConfigurationManager.
+    *
+    */
    public ConfigurationManager()
    {
       try
@@ -28,6 +49,13 @@ public class ConfigurationManager
       }
    }
 
+   /**
+    * Attempts to load the properties from the property file.
+    * 
+    * @return Returns a Properties object on success.
+    * @throws IOException
+    *            Throws an IOException on failure.
+    */
    private Properties LoadConfiguration() throws IOException
    {
       Properties properties = new Properties();
@@ -43,6 +71,7 @@ public class ConfigurationManager
 
          // Yes, you have to close an InputStream, because it is really a FileInputStream.
          // And with that, I end a multi-year debate with the Java community.
+         // Drop the mic.
          inputStream.close();
 
          // Send them back
@@ -55,6 +84,16 @@ public class ConfigurationManager
       }
    }
 
+   /**
+    * Retrieves a configuration property from the configuration property file, based on key name. If
+    * the key isn't found, it returns the default value.
+    * 
+    * @param key
+    *           The property name to load from the file.
+    * @param defaultValue
+    *           The default value to use if property can't be loaded.
+    * @return The property value on success, or the default value on failure.
+    */
    public String getConfiguration(String key, String defaultValue)
    {
       // If null, it means properties failed to load. Use default value instead.
