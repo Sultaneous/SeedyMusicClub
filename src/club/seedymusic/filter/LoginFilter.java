2e1069fb717c0fd6b03e7e86cd4cb507f5b8292d
Daniel <daniel.hong@cira.ca>
2017-11-03 21:24:48 -0400

    Added a filterName for the login web filter.

--- src/club/seedymusic/filter/LoginFilter.java
+++ src/club/seedymusic/filter/LoginFilter.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter(servletNames = {"OrderController"})
+@WebFilter(filterName="LoginFilter", servletNames = {"OrderController"})
 public class LoginFilter implements Filter{
 
 	@Override
